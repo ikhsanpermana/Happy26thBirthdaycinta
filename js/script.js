d7ec99bb550ab19643eb319556c18452540842d4
ikhsanpermana <94506205+ikhsanpermana@users.noreply.github.com>
2026-07-12 22:26:35 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,27 +29,6 @@ window.addEventListener("load", () => {
 });
 
 
-/* ============================================
-   LENIS SMOOTH SCROLL
-============================================ */
-
-const lenis = new Lenis({
-
-    duration: 1.3,
-
-    smoothWheel: true,
-
-});
-
-function raf(time) {
-
-    lenis.raf(time);
-
-    requestAnimationFrame(raf);
-
-}
-
-requestAnimationFrame(raf);
 
 
 /* ============================================
